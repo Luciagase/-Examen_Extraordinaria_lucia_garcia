05b59630899b00a0ffff2be5c501fc2f13a9fdba
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-01-23 08:59:24 +0100

    change

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -1,6 +1,16 @@
 from ast import main
 
+def potencia(base,exponente):
+    if exponente < 0:
+        return "nil"
+    else:
+        resultado = 1
+        for i in range(exponente): #Bucle que se repite el numero de veces del exponente
+            resultado = resultado * base
+        return resultado
 
+
+print(potencia(-5,3))
 
 
 if __name__ == "__main__":
