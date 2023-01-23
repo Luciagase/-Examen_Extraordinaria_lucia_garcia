e711abc2a2caab8f8630a51f8e72f4c139b09c04
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-01-23 09:32:28 +0100

    change

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,7 +1,36 @@
 from ast import main
+#Generar todas las permutaciones de la palabra dada.
 
+def permutaciones(palabra):
+    if len(palabra) <= 1:
+        return palabra
+    else:
+        tmp = []
+        for perm in permutaciones(palabra[1:]):
+            for i in range(len(perm)+1):
+                tmp.append(perm[:i] + palabra[0:1] + perm[i:])
+        return tmp
 
+print(permutaciones("AAAB"))
 
+#Eliminar las permutaciones repetidas.
+ 
+def eliminar_repetidos(lista):
+    lista.sort()
+    for i in range(len(lista)-1, 0, -1):
+        if lista[i] == lista[i-1]:
+            del lista[i]
+    return lista
+
+print(eliminar_repetidos(permutaciones("AAAB")))
+
+#Encontrar la posición partiendo de 1 de la palabra dada en la lista de permutaciones ordenadas
+
+def posicion(palabra, lista):
+    resultado = lista.index(palabra) + 1
+    print(palabra,"=",resultado)
+
+print(posicion("PREGUNTA", eliminar_repetidos(permutaciones("PREGUNTA"))))
 
 if __name__ == "__main__":
   main()
