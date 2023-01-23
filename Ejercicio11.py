bd92e7be9770f80f2fa4b0c2031045f63a5896f6
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-01-23 10:24:26 +0100

    change

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,7 +1,13 @@
 from ast import main
 
-
-
+def potencia_de_cuatro(num):
+    if type(num) != int or num <= 0:
+        return False
+    while num > 1:
+        if num % 4 != 0:
+            return False
+        num = num / 4
+    return True
 
 if __name__ == "__main__":
   main()
